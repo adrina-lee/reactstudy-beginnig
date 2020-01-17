e3681a60df47aa5b65507895dee70ebb231e6918
eunhyelee <warbleee@gmail.com>
2020-01-17 23:59:07 +0900

    클래스형에서 ref 사용하기

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import MyComponent from "./MyComponent";
 import Counter from "./Counter";
 import Say from "./Say";
 import EventPractice from "./EventPractice";
 import EventPractice2 from "./EventPractice2";
+import ValidationSample from "./ValidationSample";
+import ScrollBox from "./ScrollBox";
 
-function App() {
+class App extends Component {
+  render() {
 
-  return (
-    <>
-      <MyComponent name="마이컴포넌트" favoriteNum={13} obj={{drama: '스토브리그', '주인공': '남궁민',}}>
-        칠드런값
-      </MyComponent>
-      <Counter/>
-      <Say/>
-      <EventPractice />
-      <EventPractice2/>
-    </>
-  );
+
+    return (
+      <>
+        <div>
+          <ScrollBox ref={(ref) => this.scrollBox = ref}/>
+          <button onClick={() => this.scrollBox.scrollToBottom()}>
+            맨 밑으로
+          </button>
+        </div>
+        <ValidationSample/>
+        <MyComponent name="마이컴포넌트" favoriteNum={13} obj={{drama: '스토브리그', '주인공': '남궁민',}}>
+          칠드런값
+        </MyComponent>
+        <Counter/>
+        <Say/>
+        <EventPractice/>
+        <EventPractice2/>
+      </>
+    );
+  }
 }
 
 export default App;
