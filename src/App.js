789ddd52a7fb567f9abfc9c47b174d658822557b
eunhyelee <warbleee@gmail.com>
2020-01-17 19:29:16 +0900

    클래스형 컴포넌트와 propstype설정

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import MyComponent from "./MyComponent";
 function App() {
 
   return (
-    <MyComponent name="마이컴포넌트">
+    <MyComponent name="마이컴포넌트" favoriteNum={13} obj={{drama:'스토브리그', '주인공':'남궁민',}}>
       칠드런값
     </MyComponent>
   );
--- src/MyComponent.js
+++ src/MyComponent.js
@@ -1,19 +1,53 @@
-import React from "react";
+import React, {Component} from "react";
+import PropTypes from "prop-types";
 
-const MyComponent = props => {
+// 함수형
+// const MyComponent = ({ name, favoriteNum ,children, obj}) => {
+//
+//   return (
+//     <div>
+//       넘어온 props.name 값은 {name}입니다.<br/>
+//       children 값은 {children}입니다.<br/>
+//       좋아하는 숫자값 {favoriteNum}입니다<br/>
+//       obj값은 {JSON.stringify(obj)}
+//     </div>
+//   )
+// };
 
-  console.log(props);
+// 클래스형
+class MyComponent extends Component {
+  static  defaultProps = {
+    name: '기본 이름'
+  };
 
-  return (
-    <div>
-      넘어온 props.name 값은 {props.name}입니다.
-      children 값은 {props.children}입니다.
-    </div>
-  )
-};
+  static propType = {
+    name: PropTypes.string,
+    obj: PropTypes.object,
+    favoriteNum: PropTypes.number.isRequired,
+  };
 
-MyComponent.defaultProps = {
-  name : '기본 이름'
-};
+  render() {
+    const {name, favoriteNum, children, obj} = this.props;
+    return (
+      <div>
+        넘어온 props.name 값은 {name}입니다.<br/>
+        children 값은 {children}입니다.<br/>
+        좋아하는 숫자값 {favoriteNum}입니다<br/>
+        obj값은 {JSON.stringify(obj)}
+      </div>
+    )
+  }
+}
+
+
+// MyComponent.defaultProps = {
+//   name: '기본 이름'
+// };
+//
+// MyComponent.propType = {
+//   name: PropTypes.string,
+//   obj: PropTypes.object,
+//   favoriteNum: PropTypes.number.isRequired,
+// };
 
 export default MyComponent;
